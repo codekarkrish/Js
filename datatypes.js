3c754b75063e0973ab2576071a2f2866fb949bf5
krish chaudhary <chykrish298@gmail.com>
2026-05-02 17:24:30 +0000

    Js basics

---
+++ datatypes.js
@@ -0,0 +1,56 @@
+let a=10 //integer
+let b ="10" // string
+let c = a+b
+console.log(c);
+// boolean => true/false
+//undefined 
+let d;
+console.log(d);
+//bigint
+
+
+/* ******** Conversion *************** */
+let n = 10
+let m= "10"
+console.log(typeof n);
+console.log(typeof m);
+let x = Number(m)
+console.log(x);
+let s = "10ans"
+let t =Number(s)
+console.log(typeof t);
+//NaN
+console.log(t);
+
+// "33" =>33
+// "32ads" =>NaN
+// true => 1; false =>0
+
+let e = 1
+let f= Boolean(e)
+console.log(f);
+
+/*
+1 => true
+"krish" => true 
+0 => false
+*/
+
+/******** Operations ********** */
+/* + , - ,*, / , %  */
+console.log(1 + "2"+2);
+console.log(1+2+"2");
+console.log("1"+ 2+2);
+
+
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+
+console.log(undefined ==0);//false
+console.log(undefined >0);//false
+console.log(undefined <0);//false
+
+
+
+
