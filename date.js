f880b4e23e8457ab04f33b56664d03503e72558d
krish chaudhary <chykrish298@gmail.com>
2026-05-03 09:02:58 +0000

    dates

---
+++ date.js
@@ -0,0 +1,16 @@
+// dates 
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+
+let newdate = new Date();
+console.log(newdate);
+console.log(newdate.getDay());
+console.log(newdate.getFullYear());
+
+
+
+
