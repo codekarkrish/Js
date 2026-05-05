fa964e6b54ac3cec4f3be838f1c4156f3c3eb696
krish chaudhary <chykrish298@gmail.com>
2026-05-05 07:45:29 +0000

    objects done

--- object.js
+++ object.js
@@ -25,3 +25,37 @@ const user2 = {
 
 console.log(user2.name); // Jane
 console.log(user2[mySymbol]); // This is a symbol property
+
+
+/*** Singleton pattern */
+const user3 = {
+    fullname:{
+    name: 'Bob'},
+    age: 25
+};
+
+console.log(user3.fullname .name); // Bob
+console.log(user3.age); // 25
+
+console.log(user3);// { fullname: { name: 'Bob' }, age: 25 }
+console.log(Object.keys(user3));// [ 'fullname', 'age' ]
+console.log(Object.values(user3));// [ { name: 'Bob' }, 25 ]
+console.log(Object.entries(user3));// [ [ 'fullname', { name: 'Bob' } ], [ 'age', 25 ] ]    
+
+console.log(user3.hasOwnProperty("isLoggedIn"));// false
+
+
+
+const course = {
+    courseName: 'JavaScript',
+    duration: '3 months',
+    instructor: 'John Doe'
+};
+console.log(course.duration);// 3 months
+
+// course.courseName = 'Python'; // we can change the value of a property
+//destructuring assignment
+const { courseName, duration, instructor } = course; // destructuring assignment
+console.log(courseName);
+
+
